bc4b4ef6be9d6890e533fd28ce2dbee6bd6e88b8
Ntsako Baloyi <ntsako1616@gmail.com>
2024-10-24 19:13:50 +0200

    Creating the counter project and nav

--- src/client-side/src/app/apps/counter/counter.component.ts
+++ src/client-side/src/app/apps/counter/counter.component.ts
@@ -1,12 +1,14 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './counter.component.html',
   styleUrl: './counter.component.scss'
 })
 export class CounterComponent {
-
+  // Value on the front
+  userNumber: number = 0;
 }
