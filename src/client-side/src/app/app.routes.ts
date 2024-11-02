cd71466a58615e442223f548f19418c1c2b4858f
Ntsako Baloyi <ntsako1616@gmail.com>
2024-11-02 08:33:32 +0200

    Basic form validation

--- src/client-side/src/app/app.routes.ts
+++ src/client-side/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { CounterComponent } from './apps/counter/counter.component';
 import { AppsComponent } from './apps/apps.component';
 import { DadjokesComponent } from './apps/dadjokes/dadjokes.component';
+import { FormComponent } from './apps/form/form.component';
 
 export const routes: Routes = [
     {path: '', component: AppsComponent},
     {path: 'counter', component: CounterComponent},
-    {path: 'dadjokes', component: DadjokesComponent}
+    {path: 'dadjokes', component: DadjokesComponent},
+    {path: 'form', component: FormComponent}
 ];
---
+++ src/client-side/src/app/apps/form/form.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule,  } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss'
+})
+export class FormComponent {
+
+  // Submit
+  onSubmit(form: any){
+    console.log('From Submitted: ', form.value);
+  }
+}
