c5524b1309012b6969b29dc2e106cfd5703ad0b9
Ntsako Baloyi <ntsako1616@gmail.com>
2024-11-02 17:20:45 +0200

    Morse Code Basic Init

--- src/client-side/src/app/app.routes.ts
+++ src/client-side/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AppsComponent } from './apps/apps.component';
 import { DadjokesComponent } from './apps/dadjokes/dadjokes.component';
 import { FormComponent } from './apps/form/form.component';
 import { RandomuserComponent } from './apps/randomuser/randomuser.component';
+import { MorseComponent } from './apps/morse/morse.component';
 
 export const routes: Routes = [
     {path: '', component: AppsComponent},
     {path: 'counter', component: CounterComponent},
     {path: 'dadjokes', component: DadjokesComponent},
     {path: 'form', component: FormComponent},
-    {path: 'random-user', component: RandomuserComponent}
+    {path: 'random-user', component: RandomuserComponent},
+    {path: 'morse-code', component: MorseComponent}
 ];
