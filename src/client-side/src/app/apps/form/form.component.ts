940e959bdb215f6d3f815bf6e4257549c26ad874
Ntsako Baloyi <ntsako1616@gmail.com>
2024-11-02 09:12:32 +0200

    Fixed form validator challenge

--- src/client-side/src/app/apps/form/form.component.ts
+++ src/client-side/src/app/apps/form/form.component.ts
@@ -2,6 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule,  } from '@angular/forms';
 
+type FormData = {
+  username: string,
+  email: string,
+  password: string,
+  repassword: string
+}
+
 @Component({
   selector: 'app-form',
   standalone: true,
@@ -10,9 +17,14 @@ import { FormsModule,  } from '@angular/forms';
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
+  // Display control
+  formDisplay: boolean = true;
+  formData: FormData = {username: '',email: '', password: '', repassword: ''};
 
   // Submit
   onSubmit(form: any){
     console.log('From Submitted: ', form.value);
+    this.formData = form.value;
+    this.formDisplay = !this.formDisplay;
   }
 }
