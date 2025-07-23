f16e9c135b17d0a0ecbc83ae494ff2c7aab29ca8
Ntsako Baloyi <ntsako1616@gmail.com>
2025-07-23 21:46:02 +0200

    Remove deno

--- src/server-side/Routes/test.routes.ts
+++
@@ -1,8 +0,0 @@
-// deno-lint-ignore-file no-explicit-any no-unused-vars
-import express from "npm:express@4.18.2";
-
-export const router: any = express.Router();
-
-router.get('/ping', (req: any, res: any) => {
-    res.send('Pong');
-});
