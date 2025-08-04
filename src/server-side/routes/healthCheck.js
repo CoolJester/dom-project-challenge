077c45dc8f0fbf2494df1a9348f35323987a7803
Ntsako Baloyi <ntsako1616@gmail.com>
2025-08-04 20:07:25 +0200

    Remove node backend api

--- src/server-side/routes/healthCheck.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const healthController = require('../controllers/healthController');
-
-router.get('/healthcheck', healthController.getHealthStatus);
-router.get('/ping', healthController.getPing);
-
-module.exports = router;
