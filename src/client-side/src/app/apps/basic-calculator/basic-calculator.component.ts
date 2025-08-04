15567bff392b6bd47d6343142f7abd9b08423023
Ntsako Baloyi <ntsako1616@gmail.com>
2025-08-04 20:19:54 +0200

    Add basic calculator component and routing for new component

---
+++ src/client-side/src/app/apps/basic-calculator/basic-calculator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-calculator',
+  standalone: true,
+  imports: [],
+  templateUrl: './basic-calculator.component.html',
+  styleUrl: './basic-calculator.component.scss'
+})
+export class BasicCalculatorComponent {
+
+}
