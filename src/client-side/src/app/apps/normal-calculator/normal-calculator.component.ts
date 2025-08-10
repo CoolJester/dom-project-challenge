0d24603cd4d65a7e1d047f00e3b5b7c49cc31b8b
Ntsako Baloyi <ntsako1616@gmail.com>
2025-08-10 14:29:51 +0200

    Start with basic implementation on functionality

--- src/client-side/src/app/apps/normal-calculator/normal-calculator.component.ts
+++ src/client-side/src/app/apps/normal-calculator/normal-calculator.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-normal-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './normal-calculator.component.html',
   styleUrl: './normal-calculator.component.scss'
 })
 export class NormalCalculatorComponent {
+  // Preview for changes on the calculator
+  preview: string = '';
 
+  // Total for expression
+  total: string = '0';
+
+  // Method to add something to the preview
+  appendValue(value: string){
+    console.log('This is value clicked: ', value);
+    // Check if it's a value click and proceed only then
+    // TODO;     
+
+    // Update the preview
+    this.preview =  this.preview + value;
+
+  }
+
+  // Method to clear the preview
+  clearPreview(){
+    this.preview = '';
+  }
 }
